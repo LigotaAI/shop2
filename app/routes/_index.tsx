3f0e0fe95157a319d3dd248c2d8651c42cf10f08
Natchie Subramaniam <beta.hat@betaml.com>
2026-05-25 13:36:15 -0400

    Add root redirect to /app, disable auto URL updates on dev

---
+++ app/routes/_index.tsx
@@ -0,0 +1,6 @@
+import { redirect, type LoaderFunctionArgs } from "@remix-run/node";
+
+export const loader = ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  return redirect(`/app${url.search}`);
+};
